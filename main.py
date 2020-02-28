20acea0a6ccf6190dc1f16b4b63ea06b181b7158
Gauravism2017 <Gauravism@outlook.com>
2020-02-29 00:27:18 +0530

    'toyboy'

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 #from src.algo import selection_sort
 #from src.algo import merge_sort
 #import src.plots.convert2Video
-from src import generate_data
+# from src import generate_data
 # from src.algo import merge_sort
-from src.algo import quicksort
+# from src.algo import quicksort
 #from src.algo import quicksort
 # from src.algo import radix_sort
 # import src.graphAlgo.BFS
--- src/generate_data.py
+++ src/generate_data.py
@@ -4,31 +4,6 @@ import random
 import numpy as np
 from scipy.sparse import rand
 from src.config import SAVE_LOCATION
-<<<<<<< Updated upstream
-#   ceate a list
-# f = open('inp', 'w')
-# std = sys.stdout
-# sys.stdout = f
-#   ceate a list
-# li = [random.getrandbits(32) for i in range(20)]
-# li = str(li)[1:-1]
-# generate a matrix:
-f = open('input.txt', 'w')
-std = sys.stdout
-sys.stdout = f
-li = [random.getrandbits(256) for i in range(20)]
-li = str(li)[1:-1]
-n = 30
-# li = [[random.getrandbits(8) for i in range(n)] for i in range(n)]
-li =  rand(n, n, density=(0.6*10)/n).todense()*5
-li = li.astype(int)
-li = str(li).replace('[', ' ').replace(']', '')
-print(n)
-print(li)
-print(0)
-sys.stdout = std
-f.close()
-=======
 
 def generate(kind='list', **kwargs):
     f = open('input.txt', 'w')
@@ -50,5 +25,4 @@ def generate(kind='list', **kwargs):
         
     sys.stdout = std
     f.close()
->>>>>>> Stashed changes
     
