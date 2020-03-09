12f1c67d7562197fe78e6b7c7a4a272d3efb5a66
Gauravism2017 <Gauravism@outlook.com>
2020-03-09 07:07:28 +0530

    test

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 
 
-from src.algo import insertion_sort
-from src.algo import bubble_sort
+# from src.algo import insertion_sort
+# from src.algo import bubble_sort
 from src.algo import selection_sort
 # from src.algo import merge_sort
 #import src.plots.convert2Video
 # from src import generate_data
-from src.algo import merge_sort
+# from src.algo import merge_sort
+# # from src.algo import quicksort
 # from src.algo import quicksort
-from src.algo import quicksort
-from src.algo import radix_sort
-import src.graphAlgo.BFS
-from src.algo import heapsort
-import src.graphAlgo.DFS
-from src.graphAlgo import kruskal
-from src.graphAlgo import toposort
-from src.graphAlgo import prims
+# from src.algo import radix_sort
+# import src.graphAlgo.BFS
+# from src.algo import heapsort
+# import src.graphAlgo.DFS
+# from src.graphAlgo import kruskal
+# from src.graphAlgo import toposort
+# from src.graphAlgo import prims
 
 
